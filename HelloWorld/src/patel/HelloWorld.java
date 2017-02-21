1c1c73c43dd3f38216e76ca0f1050de9f2143f3a
Kaazim Patel <323966358@C-DS24WR1.tdsb.on.ca>
2017-02-21 11:23:38 -0500

    I am pushing hello world program.

---
+++ HelloWorld/src/patel/HelloWorld.java
@@ -0,0 +1,19 @@
+package patel;
+/**
+ * HelloWorld.java
+ * This program prints Hello, world! to the console
+ * February 21, 2017
+ * @author 323966358
+ *
+ */
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System. out.println("Hello, World!");
+
+	}
+
+}
